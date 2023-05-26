f558faef3c6bda997d9c4ad2cb2d02ab61534053
hm243695czl <1224311695@qq.com>
2023-05-26 23:23:50 +0800

    '创建多模块项目'

---
+++ hl-admin/src/main/java/cn/hl/admin/HlAdminApplication.java
@@ -0,0 +1,13 @@
+package cn.hl.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HlAdminApplication.class, args);
+    }
+
+}
---
+++ hl-admin/src/test/java/cn/hl/admin/HlAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hl.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
