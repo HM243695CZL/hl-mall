082c93a8137520fb9f12c03b4587624a61510df1
hm243695czl <1224311695@qq.com>
2023-05-29 14:29:53 +0800

    新增数据和更新数据时自动填充时间

--- hl-admin/src/main/java/cn/hl/admin/modules/ums/controller/UmsAdminController.java
+++ hl-admin/src/main/java/cn/hl/admin/modules/ums/controller/UmsAdminController.java
@@ -1,6 +1,9 @@
 package cn.hl.admin.modules.ums.controller;
 
 import cn.hl.common.api.CommonResult;
+import cn.hl.common.log.LogAnnotation;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,36 +24,47 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/admin/admin")
+@Api(tags = "用户管理", description = "用户管理")
 public class UmsAdminController {
 
     @Autowired
     private UmsAdminService umsAdminService;
 
     // 新增
+    @LogAnnotation
+    @ApiOperation("新增用户")
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public CommonResult save(@RequestBody UmsAdmin umsAdmin){
         return CommonResult.success(umsAdminService.save(umsAdmin));
     }
 
     // 更新
+    @LogAnnotation
+    @ApiOperation("更新用户")
     @RequestMapping(value = "/update", method = RequestMethod.POST)
     public CommonResult update(@RequestBody UmsAdmin umsAdmin){
         return CommonResult.success(umsAdminService.updateById(umsAdmin));
     }
 
     // 删除
+    @LogAnnotation
+    @ApiOperation("删除用户")
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public CommonResult delete(@PathVariable String id){
         return CommonResult.success( umsAdminService.removeById(id));
     }
 
     // 获取全部
+    @LogAnnotation
+    @ApiOperation("获取全部用户")
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public CommonResult list(){
         return CommonResult.success(umsAdminService.list());
     }
 
     // 查看
+    @LogAnnotation
+    @ApiOperation("查看用户")
     @RequestMapping(value = "/view/{id}", method = RequestMethod.GET)
     public CommonResult findOne(@PathVariable String id){
         return CommonResult.success(umsAdminService.getById(id));
